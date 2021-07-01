966907ea666ca2016f11e7ea9513d0c68223784e
sogogi123 <gurdlekwkd@gmail.com>
2021-07-01 16:50:21 +0900

    2775 for 구문을 두번이나 쓴것이 좀 마음에 걸린다

--- sogogi123/기본수학1/2775.py
+++ sogogi123/기본수학1/2775.py
@@ -2,8 +2,8 @@ Test_case =int(input())
 for _ in range(Test_case):
     k =int(input())
     n = int(input())
-    floor = list(range(1,15))
-    for i in range(0,k):
-        for j in range(1,n+1):
-            floor[j] += sum(floor[j-1:j])
-    print(floor[n])
+    beforefloor = list(range(1,15))
+    for i in range(k):
+        for j in range(1,n):
+            beforefloor[j] += beforefloor[j-1]
+    print(beforefloor[n-1])
