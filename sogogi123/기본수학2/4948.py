43e70c23c8dfb58f96f4b2f94934935371fc94a8
sogogi123 <gurdlekwkd@gmail.com>
2021-07-06 17:19:49 +0900

    4948 각수마다 범위의 소수를 찾는 것보다 정해진 범위의 소수에서 각범위를 찾는 것이 효율적인 코드란것을 알게해준 문제이다

---
+++ sogogi123/기본수학2/4948.py
@@ -0,0 +1,44 @@
+def Findss(n):
+    if n== 1:
+        return False
+    if n ==2:
+        return True
+    for i in range(2,int(n**0.5)+1):
+        if n%i ==0:
+            return False
+    return True
+
+
+numList = []
+for i in range(1,123456*2):
+    if Findss(i):
+        numList.append(i)
+while True:
+    num = int(input())
+    count = 0
+    if num == 0:
+        break
+    for j in numList:
+        if num < j<= num*2:
+            count +=1
+    print (count)
+
+
+
+
+
+
+
+
+# while True :
+    
+#     num = int(input())
+    
+#     numList =[]
+#     M = 2*num
+#     for i in range(num+1, M+1):
+#         if Findss(i):
+#             numList.append(i)
+#     if num == 0:
+#         break
+#     print(len(numList))
