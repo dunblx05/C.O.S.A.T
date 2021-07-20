593059ebcaf289032b3ed6871c4b701bf71cefba
sogogi123 <gurdlekwkd@gmail.com>
2021-07-20 17:39:55 +0900

    14889 집가서 수정할것

---
+++ sogogi123/백트래킹/14889.py
@@ -0,0 +1,42 @@
+import sys
+# import itertools
+
+# num = int(sys.stdin.readline())
+# arr = [list(map(int, input().split()))for row in range(num)]
+# numList = range(num)
+# pList = list(itertools.permutations(numList,num//2))
+# def dfs(i,res):
+#     if i == num//2:
+#         start,link =0,0
+
+
+
+
+    # else:
+    #     for j in range(num)
+            
+    #         dfs(i+1,arr[]
+
+import sys
+num  = int(sys.stdin.readline())
+numList =[]
+res = ""
+def make(i):
+    global numList, res
+    
+    if i ==num:
+        return numList[num-1]
+    else:
+        if 
+        res += "4"
+        numList.append(res) 
+        make(i+1)
+        if
+        res += "7"
+        numList.append(res) 
+        make(i+1)
+
+
+    
+print(make(0))
+
