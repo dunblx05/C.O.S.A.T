7888f214b847e27c068b3ac91d6a2a5b4a6835b9
sogogi123 <gurdlekwkd@gmail.com>
2021-07-26 18:17:15 +0900

    9184 메모이제이션을 이용하는 문제였다. 보기에 주어진 otherwise it returns를 else:의 뜻으로 이해해서 틀렸던 문제이다

--- sogogi123/동적계획법1/1003.py
+++ sogogi123/동적계획법1/1003.py
@@ -1,22 +1,39 @@
-import  sys
+# import  sys
 
-def fibo(n):
-    global zero_count, one_count
-    if n==0:
-        zero_count +=1
-        return 0       
-    elif n ==1:
-        one_count +=1
-        return 1        
-    else:
-        return fibo(n-1)+ fibo(n-2)
+# def fibo(n):
+#     global zero_count, one_count
+#     if n==0:
+#         zero_count +=1
+#         return 0       
+#     elif n ==1:
+#         one_count +=1
+#         return 1        
+#     else:
+#         return fibo(n-1)+ fibo(n-2)
 
-num = int(sys.stdin.readline())
+# num = int(sys.stdin.readline())
+# for i in range(num):
+#     count = 0
+#     zero_count = 0
+#     one_count =0
+#     a= int(sys.stdin.readline())
+#     fibo(a)
+#     print(zero_count, one_count)
+
+#####
+#  규칙
+# n	    0	1	2	3	4	5
+# zero	1	0	1	1	2	3
+# one	0	1	1	2	3	5
+
+num = int(input())
 for i in range(num):
-    count = 0
-    zero_count = 0
-    one_count =0
-    a= int(sys.stdin.readline())
-    fibo(a)
-    print(zero_count, one_count)
-
+    n = int(input())
+    zero =1
+    one = 0
+    temp = 0
+    for j in range(n):
+        temp = one
+        one = one + zero
+        zero = temp
+    print(zero, one)
--- sogogi123/백트래킹/9663.py
+++ sogogi123/백트래킹/9663.py
@@ -5,6 +5,7 @@ def isZero(x):
     for i  in range(x):
         if row[x] == row[i] or abs(row[x]-row[i])== x-i:
             return 0
+            
     return 1
 def dfs(n):
     global count
@@ -18,23 +19,3 @@ def dfs(n):
 
 dfs(0)
 print(count)
-
-# n = int(input())
-# s = [0 for i in range(16)]
-# result = 0
-# def isTrue(x):
-#     for i in range(1, x):
-#         if s[x] == s[i] or abs(s[x] - s[i]) == x - i:
-#             return False
-#     return True
-# def dfs(cnt):
-#     global result
-#     if cnt > n:
-#         result += 1
-#     else:
-#         for i in range(1, n + 1):
-#             s[cnt] = i
-#             if isTrue(cnt):
-#                 dfs(cnt + 1)
-# dfs(1)
-# print(result)
