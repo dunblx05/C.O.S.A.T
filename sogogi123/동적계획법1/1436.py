1566e1eef92ffc333453f8794a036230db7d601b
sogogi123 <gurdlekwkd@gmail.com>
2021-08-01 17:56:02 +0900

    1436 처음에는 조건문으로 풀다가 10이라는 반례가 있어 동적할당으로 풀기 시작하였다. 2와 3으로 나누고 count[1]에는 0이 들어 있어야하므로 이것을 생각해 주는 게 어려웠다

---
+++ sogogi123/동적계획법1/1436.py
@@ -0,0 +1,15 @@
+import sys
+num = int(sys.stdin.readline())
+count = [0 for _ in range(num+1)]
+
+for i in range(2,num+1):
+    count[i] = count[i-1]+1
+
+    if i %2==0 and count[i]> count[i//2]+1:
+        count[i] = count[i//2]+1
+    if i %3==0 and count[i]> count[i//3]+1:
+        count[i] = count[i//3]+1
+
+
+
+print(count[num])
