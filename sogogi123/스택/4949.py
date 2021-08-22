593744d3f899b1a0c52db2f1cb6180f69c022b7b
sogogi123 <gurdlekwkd@gmail.com>
2021-08-22 20:57:16 +0900

    4949 스택을 이용하는 문제 괄호를 닫을때 not 을 이용하여 리스트가 원래 비었다면 False 조건을 주는 것을 생각하지 못하여 헤메었던 문제

---
+++ sogogi123/스택/4949.py
@@ -0,0 +1,26 @@
+import sys
+while True:
+    string = sys.stdin.readline().rstrip()
+    if string == ".":
+        break
+    res = None
+    stack = []
+    for i in string:
+        if i =="(" or i=="[":
+            stack.append(i)
+        elif i==")":
+            if not stack or stack[-1]=="[":
+                res = False
+                break
+            elif stack[-1] =="(":
+                stack.pop()
+        elif i=="]":
+            if not stack or stack[-1]=="(": #not list 의 형태는 list 안이 비었는지 확인해 준다 not list =True 라면 비었다는 뜻
+                res = False
+                break
+            elif stack[-1] =="[":
+                stack.pop()
+    if res ==True and not stack:
+        print("yes")
+    else:
+        print("no")    
